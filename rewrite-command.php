e0e136639cd82ae1df9ab1e8b08a4894cae25aef
William Patton <will@pattonwebz.com>
2019-04-18 02:53:28 +0100

    PHPCS: prefix global variables

--- rewrite-command.php
+++ rewrite-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_rewrite_autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_rewrite_autoload ) ) {
+	require_once $wpcli_rewrite_autoload;
 }
 
 WP_CLI::add_command( 'rewrite', 'Rewrite_Command' );
