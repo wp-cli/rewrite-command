293f9de9905b9d0199d72ff0d17e837228e47a10
Alain Schlesser <alain.schlesser@gmail.com>
2023-08-30 17:25:42 +0200

    Update to WPCS v3 (#63)
    * Update to wp-cli-tests v4 (which requires WPCS v3)
    
    * Fix all autofixable CS issues
    
    * Ignore error
    
    ---------
    
    Co-authored-by: Pascal Birchler <pascalb@google.com>

--- rewrite-command.php
+++ rewrite-command.php
@@ -4,7 +4,7 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$wpcli_rewrite_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+$wpcli_rewrite_autoloader = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $wpcli_rewrite_autoloader ) ) {
 	require_once $wpcli_rewrite_autoloader;
 }
